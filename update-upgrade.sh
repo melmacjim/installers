072d14380126de0b5bc9cfcf982931cd264ca8f9
melmacjim <6190157+melmacjim@users.noreply.github.com>
2019-09-23 22:31:05 -0400

    Added update-upgrade.sh

---
+++ update-upgrade.sh
@@ -0,0 +1,42 @@
+#! /bin/bash
+
+run_checks () {
+  if ! ping -c1 -q google.com > /dev/null ; then
+    echo "No internet connection."
+    exit 1
+  elif ! id | grep -q root ; then
+    echo "Not root! (try sudo $0)"
+    exit 1
+  fi
+}
+
+update_upgrade () {
+  run_checks
+  printf "\nCHECKING FOR SOFTWARE UPDATES ...\n\n"
+  apt update && apt upgrade ${2:+$2}
+}
+
+update_dist_upgrade () {
+  run_checks
+  printf "\nCHECKING FOR KERNEL UPDATES ...\n\n"
+  apt update && apt dist-upgrade ${2:+$2}
+}
+
+update_autoremove () {
+  run_checks
+  printf "\nCHECKING FOR REMOVABLE SOFTWARE ...\n\n"
+  apt update && apt autoremove ${2:+$2}
+}
+
+if [ -z "$1" ] ; then
+  update_upgrade
+else
+  for input_options ; do
+    case "$input_options" in
+      -u|--update) update_upgrade $@ ;;
+      -k|--kernel) update_dist_upgrade $@ ;;
+      -r|--autoremove) update_autoremove $@ ;;
+    esac
+  done
+fi
+
