d2ec8cd58174f47daac67f6f0405d6df1397706f
melmacjim <6190157+melmacjim@users.noreply.github.com>
2019-12-25 17:30:54 -0500

    install-cubic.sh - added a check to see if cubic is ready installed.

--- install-cubic.sh
+++ install-cubic.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-if uname -a | grep -i ubuntu ; then
-  apt-add-repository ppa:cubic-wizard/release
-  apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 6494C6D6997C215E
-  apt update && apt install cubic
+if [ -f ] ; then
+  echo "Looks like cubic in already installed on this system. ;)"
 else
-  echo "Sorry but this script only works on Ubuntu."
+  if uname -a | grep -i ubuntu ; then
+    apt-add-repository ppa:cubic-wizard/release
+    apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 6494C6D6997C215E
+    apt update && apt install cubic
+  else
+    echo "Sorry but this script only works on Ubuntu."
+  fi
 fi
+
