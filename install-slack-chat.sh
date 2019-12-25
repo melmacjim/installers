6b64127c951940842a7932cc64fed38c33114bd4
melmacjim <6190157+melmacjim@users.noreply.github.com>
2019-12-25 17:28:12 -0500

    Added `install-slack-chat.sh` to this repo.

---
+++ install-slack-chat.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ -f /usr/lib/slack/slack ] ; then
+  echo "Looks like slack for the desktop is already installed. :)"
+else
+  if uname -a | grep -i "ubuntu\|debian" ; then
+    cd /tmp
+    rm slack-desktop-*.deb*
+    wget https://downloads.slack-edge.com/linux_releases/slack-desktop-4.0.2-amd64.deb
+    apt update && apt install -y ./slack-desktop-*.deb
+  else
+    echo "Sorry but this script only works on Debain based distros."
+  fi
+fi
+
