504eaffc752f7f322f87c661d7073558d69090b8
melmacjim <6190157+melmacjim@users.noreply.github.com>
2025-05-03 00:08:34 -0400

    added some ansible stuff

--- install-slack-chat.sh
+++ install-slack-chat.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+PKG_NAME="slack-desktop-4.0.2-amd64.deb"
+PKG_URL="https://downloads.slack-edge.com/linux_releases/${PKG_NAME}"
+
 if [ -f /usr/lib/slack/slack ] ; then
   echo "Looks like slack for the desktop is already installed. :)"
 else
   if uname -a | grep -i "ubuntu\|debian" ; then
     cd /tmp
-    rm slack-desktop-*.deb*
-    wget https://downloads.slack-edge.com/linux_releases/slack-desktop-4.0.2-amd64.deb
-    apt-get update && apt-get install -y ./slack-desktop-*.deb
+    rm -f ${PKG_URL}
+    wget ${PKG_URL}
+    echo "apt-get update && apt-get install -y ./${PKG_NAME}"
   else
     echo "Sorry but this script only works on Debain based distros."
   fi
